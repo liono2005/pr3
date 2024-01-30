f103ab0e2c866b7e455a4add42c10e0ffe90622d
rejme <git@github.com:liono2005/PR1.git>
2024-01-30 21:02:36 +0300

    rtth

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,5 +11,14 @@ public class Main {
         if (A > -137 && A < -57 || A > 123 && A < 55) {
             System.out.println("Принадлежит интервалу.");
         } else System.out.println("Не принадлежит интервалу.");
+       //pflfybt 3.2
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        if ((A % 2 == 0 || B % 2 == 0) && (A % 3 == 0 && B % 3 == 0 && C % 3 == 0)) {
+            System.out.println("Истина");
+        } else System.out.println("Ложь");
     }
     }
+
